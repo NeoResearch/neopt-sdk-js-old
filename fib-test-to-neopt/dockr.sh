461943da5cd8b99ad9e9425411293dcf4bdcae7d
Igor Machado <igor.machado@gmail.com>
2020-05-27 18:55:13 -0300

    docker script working for fib

---
+++ fib-test-to-neopt/dockr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -s EXPORTED_FUNCTIONS="['_mytest', '_myteststr']"
+# --std=c++17
+
+#docker run --rm -v $(pwd)/../:/src trzeci/emscripten em++   -s EXTRA_EXPORTED_RUNTIME_METHODS='[\"cwrap\"]' -g -O3 -Wall -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH=1       --js-library src/neo3-cpp-bindings/general-libcore_exports.js -I./thirdparty/neo3-cpp-core/src/ -I./thirdparty/neo3-cpp-core/libs/ -I./src/ -O3 -s WASM=1  -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s 'EXPORT_NAME=\"ourtest\"' -o ./fib-test-to-neopt/ourtest.js ./fib-test-to-neopt/neopt-test2.cpp
+
+# -s EXTRA_EXPORTED_RUNTIME_METHODS='[\"cwrap\"]'
+
+#-s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'UTF8ToString', 'stringToUTF8', 'writeStringToMemory', 'getValue', 'setValue']"
+
+
+docker run --rm -v $(pwd)/../:/src trzeci/emscripten em++ -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s 'EXPORT_NAME="ourtest"' -o ./fib-test-to-neopt/ourtest.js ./fib-test-to-neopt/neopt-test2.cpp
