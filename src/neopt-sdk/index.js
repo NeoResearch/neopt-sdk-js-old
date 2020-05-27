f7d473f0902eb27b19dc08396b13cca3b178cb5c
Igor Machado <igor.machado@gmail.com>
2020-05-26 21:19:45 -0300

    verbose

--- src/neopt-sdk/index.js
+++ src/neopt-sdk/index.js
@@ -1,9 +1,14 @@
 // Neo3 Portable for all Things (neopt) SDK in Javascript
 
 const Neo3 = require('./Neo3').Neo3;
+//import Neo3 from './Neo3';
 
-let CppModule = require('neopt-lib-node-cpp');
-let csbig = require('csbiginteger');
+const CppModule = require('neopt-lib-node-cpp');
+const csbig = require('csbiginteger');
+
+//console.log("MODULE:"+CppModule);
+//console.log("FUNC: "+CppModule._mytest);
+//CppModule._mytest(1);
 
 module.exports = {
   Neo3,
