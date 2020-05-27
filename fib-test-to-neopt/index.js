fb95339a16d4485c0026b7e83f6488776d33afe9
Igor Machado <igor.machado@gmail.com>
2020-05-27 20:00:32 -0300

    exporting module

--- fib-test-to-neopt/index.js
+++ fib-test-to-neopt/index.js
@@ -20,7 +20,7 @@ import ourtestWasm from './ourtest.wasm';
 // `.wasm` file, we have to provide a `locateFile()` hook to redirect
 // to the appropriate URL.
 // More details: https://kripken.github.io/emscripten-site/docs/api_reference/module.html
-const mymodule = ourtest({
+export const mymodule = ourtest({
   locateFile(path) {
     if(path.endsWith('.wasm')) {
       return ourtestWasm;
