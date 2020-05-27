1d26788cf3b1dfa2f3f9de6ec1766a104b158677
Igor Machado <igor.machado@gmail.com>
2020-05-26 22:26:08 -0300

    exporting unexisting function

--- src/neopt-sdk/index.js
+++ src/neopt-sdk/index.js
@@ -1,3 +1,4 @@
+"use strict";
 // Neo3 Portable for all Things (neopt) SDK in Javascript
 
 const Neo3 = require('./Neo3').Neo3;
@@ -8,6 +9,9 @@ const csbig = require('csbiginteger');
 
 const CppModule = require('neopt-lib-node-cpp');
 
+const cppFunc = require('neopt-lib-node-cpp').cpp_SmartContract_Contract_CreateSignatureRedeemScript;
+const cppFunc2 = require('neopt-lib-node-cpp').cpp_SmartContract_Contract_CreateSignatureRedeemScript2;
+
 //console.log("MODULE:"+CppModule);
 //console.log("FUNC: "+CppModule._mytest);
 //CppModule._mytest(1);
@@ -15,6 +19,8 @@ const CppModule = require('neopt-lib-node-cpp');
 module.exports = {
   Neo3,
   CppModule, // emscripten Module
+  cppFunc, // function
+  cppFunc2, // function
   csbig
 }
 
